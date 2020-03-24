46a2cba746606523cb247b1b6eacc77ea45ffd2c
felipends <dasilvajfelipe@gmail.com>
2020-03-23 22:38:15 -0300

    pegando tweets e adicionando ao aqruivo results/result.json

---
+++ README.md
@@ -0,0 +1,12 @@
+# TIL - Tweet scraper
+
+Projeto para capturar os meus tweets que começam com TIL, onde eu registro o que aprendi no dia.
+
+----
+
+## Tecnologias Usadas
+
+	1. twitterscraper [`Disponível aqui`_]
+	2. JSON (Python builtin)
+
+.. _Disponível aqui: https://github.com/taspinar/twitterscraper
---
+++ src/main.py
@@ -0,0 +1,26 @@
+from twitterscraper.query import query_single_page
+from twitterscraper.tweet import Tweet
+import json
+
+class ScrappedTweet:
+    def __init__(self, text, timestamp):
+        self.text = text
+        self.timestamp = str(timestamp)
+
+def write_infile(text, timestamp):
+    st = ScrappedTweet(text, timestamp)
+    try:
+        file_output = open("../results/result.json", "a+")
+        file_output.write(json.dumps(st.__dict__))
+    except OSError:
+        print(e)
+        return
+
+if __name__ == "__main__":
+    list_of_tweets = query_single_page("felipendsdev", "", None, from_user=True)
+    for tweet in list_of_tweets:
+        for t in tweet:
+            if type(t) == Tweet:
+                if t.text.startswith("TIL:"):
+                    print(t.timestamp.date())
+                    write_infile(t.text, t.timestamp.date())
